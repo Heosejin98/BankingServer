16b76f828f7c04f1b5566f8c0035907f5976a666
Heosejin98 <sejin3319@naver.com>
2023-02-14 20:11:02 +0900

    feat: setup Banking project

---
+++ src/main/java/com/sejin/bankingsever/BankingseverApplication.java
@@ -0,0 +1,13 @@
+package com.sejin.bankingsever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingseverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingseverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sejin/bankingsever/BankingseverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejin.bankingsever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingseverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
